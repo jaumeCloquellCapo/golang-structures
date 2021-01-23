b273bb5bc8d59f82daf80fe3144551c6b16a65f2
jaume <jaumecloquell@gmail.com>
2021-01-23 18:59:37 +0100

    added test linked list

--- structures/LinkedList_test.go
+++ structures/LinkedList_test.go
@@ -4,14 +4,20 @@ import (
 	"testing"
 )
 
-func TestSinglyLinked_GetFirstValue(t *testing.T) {
+func TestLinkedList_CreateLinkedList(t *testing.T) {
 	list := NewLinkedList()
 
 	_, err := list.GetFirstValue()
+
 	if err == nil {
 		t.Errorf(err.Error())
 	}
 
+}
+
+
+func TestLinkedList_GetFirstValue(t *testing.T) {
+	list := NewLinkedList()
 
 	list.Add("abc")
 
@@ -42,8 +48,43 @@ func TestSinglyLinked_GetFirstValue(t *testing.T) {
 	if err != nil {
 		t.Errorf(err.Error())
 	}
+
 	if value != "xyz" {
 		t.Errorf("Expected xyz get %s", value)
 	}
 
 }
+
+func TestLinkedList_GetLastValue(t *testing.T) {
+	list := NewLinkedList()
+
+	list.Add("abc")
+
+	value, err := list.GetLastValue()
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if value != "abc" {
+		t.Errorf("Expected def abc %s", value)
+	}
+
+	list.Add("def")
+
+	value, err = list.GetLastValue()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if value != "def" {
+		t.Errorf("Expected def get %s", value)
+	}
+
+	list.InsertAt(2, "xyz")
+
+	value, err = list.GetLastValue()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
+
